72b75331e8139c800db1ba6c6dc79b3c1635dc38
Coulibaly N'golo Issa <coulibalyissa956@gmail.com>
2025-01-02 04:56:39 +0000

    add post list and post detail display view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest, HttpResponse
+from models import Post
 
-# Create your views here.
+def post_list(request: HttpRequest) -> HttpResponse:
+    posts: Post = Post.published.all()
+
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts}
+    )
+
+
+def post_detail(request: HttpRequest, id: int) -> HttpResponse:
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status=Post.Status.PUBLISHED
+    )
+
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post}
+    )
